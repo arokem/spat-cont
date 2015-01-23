4b25b91c4635dfd00355c69ab9ad6e94c0503e5d
arokem <arokem@gmail.com>
2015-01-22 16:50:53 -0800

    First pass of the OO implementation of the SCM.
    And now to test it out on some data!

--- scm/scm.py
+++ scm/scm.py
@@ -7,11 +7,14 @@ extent, and that we can make assumptions about the data in voxel 0, based on
 its neighbors on all sides. 
 
 """
+import numpy as np
 import utils as ut
 import dipy.reconst.sfm as sfm
 import dipy.core.geometry as geo
 from dipy.reconst.sfm import SparseFascicleModel, SparseFascicleFit
 from dipy.core.onetime import auto_attr
+import dipy.core.ndindex as dnd
+
 
 class SpatContModel(SparseFascicleModel):
     def __init__(self, gtab, sphere=None, response=[0.0015, 0.0005, 0.0005],
@@ -19,6 +22,7 @@ class SpatContModel(SparseFascicleModel):
         
         SparseFascicleModel.__init__(self, gtab, sphere, response,
                                      solver, l1_ratio, alpha)
+        self.orig_alpha = alpha
 
     @auto_attr
     def design_matrix(self):
@@ -31,11 +35,12 @@ class SpatContModel(SparseFascicleModel):
         Parameters
         ----------
         data : array
-            The measured signal from one voxel.
+            The measured signal.
 
-        mask : array
+        mask : array, optional
             A boolean array used to mark the coordinates in the data that
-            should be analyzed that has the shape data.shape[:-1]
+            should be analyzed. Has the shape `data.shape[:-1]`. Default: the
+            entire volume, except the edges.
 
         Returns
         -------
@@ -43,40 +48,39 @@ class SpatContModel(SparseFascicleModel):
 
         """
         if mask is None:
-            flat_data = np.reshape(data, (-1, data.shape[-1]))
-        else:
-            mask = np.array(mask, dtype=bool, copy=False)
-            flat_data = np.reshape(data[mask], (-1, data.shape[-1]))
+            mask = np.ones(data.shape)
+        # Pre-allocate the final result:
+        beta = np.zeros(data.shape[:-1] + (self.design_matrix.shape[-1], ))
+        # We will go voxel by voxel, avoiding the edges altogether:
+        coords = np.array(list(dnd.ndindex((data.shape[0]-2,
+                                            data.shape[1]-2,
+                                            data.shape[2]-2)))) + 1
 
-        # Fitting is done on the relative signal (S/S0):
-        flat_S0 = np.mean(flat_data[..., self.gtab.b0s_mask], -1)
-        flat_S = flat_data[..., ~self.gtab.b0s_mask] / flat_S0[..., None]
-        flat_mean = np.mean(flat_S, -1)
-        flat_params = np.zeros((flat_data.shape[0],
-                                self.design_matrix.shape[-1]))
+        S0 = np.mean(data[..., self.gtab.b0s_mask], -1)
+        # Normalize and remove mean:
+        norm_data = (data[..., ~self.gtab.b0s_mask] / S0[..., None])
+        mean_data = np.mean(norm_data, -1)
+        norm_data = norm_data - mean_data[..., None]
+        # Weights for the 3x3 matrix of voxels around the voxel of interest,
+        # calculated by the distance from the center of the central voxel:
+        dist_weights = ut.signal_weights(self.gtab)
+        dw_shape = np.sum(~self.gtab.b0s_mask)
+        
+        for i, j, k in coords:
+            local_mask = mask[i-1:i+2, i-1:i+2, i-1:i+2]
+            local_data = norm_data[i-1:i+2, i-1:i+2, i-1:i+2]
+            local_dist_weight = (dist_weights * local_mask[..., None]).ravel()
+            local_dm = (self.design_matrix * local_mask[..., None, None])
+            local_dm = local_dm.reshape(-1,self.design_matrix.shape[-1])
+            fit_it = local_data.ravel() * local_dist_weight
+            this_alpha = (self.orig_alpha *
+                          np.sum(local_dist_weight**2)/local_dm.shape[0])
+            print this_alpha
+            self.solver.alpha = this_alpha
+            beta[i, j, k] = self.solver.fit(local_dm,
+                                            fit_it).coef_
 
-        for vox, vox_data in enumerate(flat_S):
-            if np.any(np.isnan(vox_data)):
-                flat_params[vox] = (np.zeros(self.design_matrix.shape[-1]))
-            else:
-                fit_it = vox_data - flat_mean[vox]
-                flat_params[vox] = self.solver.fit(self.design_matrix,
-                                                   fit_it).coef_
-        if mask is None:
-            out_shape = data.shape[:-1] + (-1, )
-            beta = flat_params.reshape(out_shape)
-            mean_out = flat_mean.reshape(out_shape)
-            S0 = flat_S0.reshape(out_shape).squeeze()
-        else:
-            beta = np.zeros(data.shape[:-1] +
-                            (self.design_matrix.shape[-1],))
-            beta[mask, :] = flat_params
-            mean_out = np.zeros(data.shape[:-1])
-            mean_out[mask, ...] = flat_mean.squeeze()
-            S0 = np.zeros(data.shape[:-1])
-            S0[mask] = flat_S0
-
-        return SpatContFit(self, beta, S0, mean_out.squeeze())
+        return SpatContFit(self, beta, S0, mean_data)
 
         
 class SpatContFit(SparseFascicleFit):
--- scm/utils.py
+++ scm/utils.py
@@ -5,6 +5,8 @@ import dipy.core.geometry as geo
 import dipy.core.ndindex as dnd
 
 TAU = 1
+NEIGHBORS = 3
+
 
 def l2norm(vector):
     """
@@ -44,32 +46,35 @@ def weighting(location, out_dir, in_dir, tau=TAU, exp=0.01):
 
 
 def design_matrix(gtab, sphere, evals=np.array([0.0015, 0.0005, 0.0005])):
-    """ 
+    """
+    The volumized design matrix by voxel in the NEIGHBORS-by-NEIGHBORS neighborhood
+    
 
     """
     # Get the generic SFM design matrix:
     sfm_dm = sfm.sfm_design_matrix(gtab, sphere, response=evals)
     # Initialize it with the V0 matrix (which is the original SFM matrix):
-    dm = []
-    coords = np.array(list(dnd.ndindex((3, 3, 3)))) - 1
+    dm = np.zeros((NEIGHBORS, NEIGHBORS, NEIGHBORS,
+                   np.sum(~gtab.b0s_mask), sphere.x.shape[0]))
+    coords = np.array(list(dnd.ndindex((NEIGHBORS, NEIGHBORS, NEIGHBORS)))) - 1
     rows = np.arange(np.sum(~gtab.b0s_mask))
     columns = np.arange(sphere.x.shape[0])
+    dm[1, 1, 1] = sfm_dm
     for x, y, z in coords:
-        if [x, y, z] == [0, 0, 0]:
-            dm.append(sfm_dm)
+        location = np.array([x, y, z])
+        if np.all(location == np.array([0, 0, 0])):
+            pass
         else:
             # Start with the original matrix and downweight each
             # component as necessary:
-            this_dm = sfm_dm.copy() 
-            location = np.array([x, y, z])
             for row in rows:
                 out_dir = gtab.bvecs[~gtab.b0s_mask][row]
                 for col in columns:
                     in_dir = sphere.vertices[col]
-                    this_dm[row, col] *=\
-                        weighting(location, out_dir, in_dir)
-            dm.append(this_dm)
-    return np.concatenate(dm)
+                    dm[location[0]+1, location[1]+1, location[2]+1, row, col] =\
+                       weighting(location, out_dir, in_dir) * sfm_dm[row, col]
+            
+    return dm
 
 
 def signal_weights(gtab, tau=TAU):
@@ -78,8 +83,8 @@ def signal_weights(gtab, tau=TAU):
     of the voxels from the center voxel).
     """
     dw_shape = np.sum(~gtab.b0s_mask)
-    dist_weights = np.zeros((3, 3, 3, dw_shape))
-    coords = np.array(list(dnd.ndindex((3, 3, 3)))) - 1
+    dist_weights = np.zeros((NEIGHBORS, NEIGHBORS, NEIGHBORS, dw_shape))
+    coords = np.array(list(dnd.ndindex((NEIGHBORS, NEIGHBORS, NEIGHBORS)))) - 1
     for x, y, z in coords:
         location = np.array([x, y, z])
         this = np.ones(dw_shape)
@@ -89,20 +94,20 @@ def signal_weights(gtab, tau=TAU):
             this *= distance_weight(np.dot(location, location), tau=tau)
         # We want this to match the array conventions in preprocess_signal:
         dist_weights[location[0]+1, location[1]+1, location[2]+1] = this
-    return dist_weights.ravel()
+    return dist_weights
 
 
 def preprocess_signal(data, gtab, i, j, k, dist_weights=None, tau=TAU):
     if dist_weights is None:
         dist_weights = signal_weights(gtab, tau=tau)
     dw_shape = np.sum(~gtab.b0s_mask)
-    sig = np.zeros(27 * dw_shape)
-    coords = np.array(list(dnd.ndindex((3, 3, 3)))) - 1
+    sig = np.zeros((NEIGHBORS ** 3) * dw_shape)
+    coords = np.array(list(dnd.ndindex((NEIGHBORS, NEIGHBORS, NEIGHBORS)))) - 1
     this_data = data[i + coords[:, 0], j + coords[:, 1], k + coords[:, 2]]
     sig = (this_data[..., ~gtab.b0s_mask] /
            np.mean(this_data[..., gtab.b0s_mask], -1)[..., None])
     sig = sig - np.mean(sig, -1)[..., None]
-    sig = sig.ravel() * dist_weights
+    sig = sig.ravel() * dist_weights.ravel()
     return sig
 
 
